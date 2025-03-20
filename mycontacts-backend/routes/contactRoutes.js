7db6e686345c99dfc3fc391473fe4c183373e401
Dhanush <76605489+dhanushsanand@users.noreply.github.com>
2025-03-20 06:05:18 +0000

    added multiple routes

--- mycontacts-backend/routes/contactRoutes.js
+++ mycontacts-backend/routes/contactRoutes.js
@@ -1,1 +1,34 @@
-const express
+const express = require('express');
+const router = express.Router();
+
+router.route('/').get((req,res)=>{
+    res.status(200).json({
+        message: "Get all Contacts from route"
+    })
+});
+
+router.route('/').post((req,res)=>{
+    res.status(200).json({
+        message: "Create Contacts from route"
+    })
+});
+
+router.route('/:id').get((req,res)=>{
+    res.status(200).json({
+        message: `Get contact for ${req.params.id}`
+    })
+});
+
+router.route('/:id').put((req,res)=>{
+    res.status(200).json({
+        message: `Updated Contact for ${req.params.id}`
+    })
+});
+
+router.route('/:id').delete((req,res)=>{
+    res.status(200).json({
+        message: `Deleted Contact for ${req.params.id}`
+    })
+});
+
+module.exports = router;
--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -4,11 +4,12 @@ const dotenv = require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/api/contacts',(req,res)=>{
-    res.status(200).json({
-        message:"Get new contacts"
-    })
-})
+app.use('/api/contacts',require("./routes/contactRoutes"))
+// app.get('/api/contacts',(req,res)=>{
+//     res.status(200).json({
+//         message:"Get new contacts"
+//     })
+// })
 
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`);
